bef6cae25b0edf6ba1e915d01bce1d17d93ba7a9
Loicavenel <lavenel@redhat.com>
2015-02-24 17:30:37 +0000

    Update service_request_rejected.rb
    Fixed :info to 'info'

--- Automate/CloudFormsPOC/Service/Provisioning/StateMachines/ServiceProvisionRequestQuotaVerification.class/__methods__/service_request_rejected.rb
+++ Automate/CloudFormsPOC/Service/Provisioning/StateMachines/ServiceProvisionRequestQuotaVerification.class/__methods__/service_request_rejected.rb
@@ -4,5 +4,5 @@
 # Description: This method runs when the service request quota validation has failed
 #
 
-$evm.log(:info, "Request denied because of Quota")
+$evm.log('info, "Request denied because of Quota")
 $evm.root["miq_request"].deny("admin", "Quota Exceeded")
